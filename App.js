7ebce1206b54ce3851b682deba563bb7235f716e
Gabriel Pereira Pinheiro <gabriel.pereira.pinheiro@gmail.com>
2020-06-24 07:21:26 -0300

    feat folders

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/header';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header ></Header>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
